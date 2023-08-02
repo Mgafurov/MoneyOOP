4e7eab7b804bb1decadea05fb3ddd364b6674a44
posey <posey@MSI>
2023-08-02 13:04:36 +0500

    Добавьте файлы проекта.

---
+++ MoneyOOP/Money.cs
@@ -0,0 +1,53 @@
+﻿public class Money
+{
+    //закрытые атрибуты для хранения рублей и копеек
+    private int rubles;
+    private int kopeks;
+    //статичная переменная для подсчёта количества созданных объектов
+    private static int totalObjects = 0;
+    public Money(int rubles, int kopeks)
+    {
+        this.rubles = rubles;
+        this.kopeks = kopeks;
+        if (this.kopeks >= 100) //Убеждаемся что копеек не может быть больше 99
+        {
+            this.rubles += this.kopeks / 100;
+            this.kopeks %= 100;
+        }
+        totalObjects++; //При создании объекта увеличиваем счётчик
+    }
+    public override string ToString()
+    {
+        return $"{rubles} руб. {kopeks} коп.";
+    }
+    public void AddKopeks(int addToKopeks)
+    {
+        //добавляем копейки с учётом переноса рублей если есть необходимость
+        this.kopeks += addToKopeks;
+        if (this.kopeks >= 100)
+        {
+            this.rubles += this.kopeks / 100;
+            this.kopeks %= 100;
+        }
+    }
+    public void SubtractKopeks(int kopeksToSubtract)
+    {
+        //вычитываем копейки с учётом переноса рубля при необходимости
+        int totalKopeks = rubles * 100 + kopeks;
+        totalKopeks -= kopeksToSubtract;
+        if (totalKopeks < 0)
+        {
+            this.rubles = 0;
+            this.kopeks = 0;
+        }
+        else
+        {
+            this.rubles = totalKopeks / 100;
+            this.kopeks = totalKopeks % 100;
+        }
+    }
+    public static int GetTotalObjects()
+    {
+        return totalObjects;
+    }
+}
---
+++ MoneyOOP/Program/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Money money1 = new Money(10, 50);
+        Money money2 = new Money(5, 75);
+        //Выводим информацию о созданных объектах
+        Console.WriteLine("Объект 1: " + money1);
+        Console.WriteLine("Объект 2: " + money2);
+        //Добавим копеек к объекту 1
+        money1.AddKopeks(80);
+        Console.WriteLine("Объект 1 (после добавления копеек): " + money1);
+        //Вычитаем копейки с объекта 1
+        money2.SubtractKopeks(30);
+        Console.WriteLine("Объект 2 (после вычитания копеек): " + money2);
+        //Считаем кол-во созданных объектов с помощью статической функции
+        int totalObjects = Money.GetTotalObjects();
+        Console.WriteLine("Кол-во созданных объектов: " + totalObjects);
+        Console.ReadKey();
+    }
+}
